2bc0e43ce8d92d81a9b63d5fcfd00b37fd4412f3
Felixdiamond <diamondfelix006@gmail.com>
2023-12-16 18:29:41 +0100

    day 09

---
+++ 09-Dev_Tools/index.js
@@ -0,0 +1,87 @@
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
+const main = async () => {
+console.log("Finally got here, I've been waiting forever😪")
+await sleep(2000)
+console.log("Oh wait, I wasn't supposed to say that out loud🤐, let's try again");
+await sleep(2000)
+console.clear();
+console.log("Hey and welcome to to the console, I'm glad you made it here😁 %cfinally", "text-decoration: line-through; color: rgba(128, 128, 128, 0.3);")
+await sleep(2000)
+console.log("I'm a %cconsole.log() %cfunction, I'm used to print out stuff to the console", "font-family: 'Courier New';", "font-family: monospace;");
+await sleep(2000)
+console.log("And today, I'm gonna tell you all about me and my console friends")
+await sleep(2000)
+console.log("So let's get started");
+await sleep(1000)
+console.clear();
+console.log("First of all, I'm a %cconsole.log() %cfunction, I'm used to print out stuff to the console", "font-family: 'Courier New';", "font-family: monospace;");
+await sleep(1000)
+console.log("The contents to be printed out are written inside my parenthesis. Stuffs written inside my parenthesis are called %carguments", "font-family: monospace;")
+await sleep(2000)
+console.log("We'll come back to arguments later, but for now, try to write something inside my parenthesis and see what happens e.g %cconsole.log('Hello World')", "font-family: monospace;")
+await sleep(2000)
+console.log("You start with the %cconsole %cobject, then a dot, then the %clog %cmethod, then parenthesis, then inside the parenthesis, you write whatever you want to print out", "font-family: 'Courier New';", "font-family: monospace;", "font-family: 'Courier New';", "font-family: monospace;")
+await sleep(1000)
+console.log("So it looks something like this %cconsole.log('Hello World').%cNow, try it out, write in the console👇🏼", "font-family: 'Courier New';", "font-family: monospace;")
+await sleep(7000)
+console.log("That should've been enough time to try me out")
+await sleep(2000)
+console.log("Now, let's talk about %carguments", "font-family: monospace;")
+await sleep(2000)
+console.log("Arguments are the contents inside my parenthesis, the stuffs that i print out are one. There are also others like styling arguments to further style what is being logged out. they are seperated with commas")
+await sleep(2000)
+console.log("Like this is a %cblue text, %cthis is a red text, %cand %cthis is a purple text. %cGojo Satoru :)", "color: blue", "color: red;", "color: white;", "color: purple;", "color: rgba(250, 250, 250, 0.05); font-family: 'Courier New';")
+await sleep(2000)
+console.log("Now you know about me, let's go to my friends")
+await sleep(2000)
+console.clear()
+console.log("Type the respective commands for all my friends. psst, the commands are the one with parentheses at their ends")
+console.log("1. Interpolation - enterone()\n2. Warning - entertwo()\n3. Error - enterthree()\n4. Info - enterfour()\n5. Testing - enterfive()\n6. Cleaning - entersix()\n....more coming up soon")
+}
+
+main()
+
+const enterone = async () => {
+    console.log("knocking...")
+    await sleep(2000)
+    console.log("Heyaa, I'm Interpolation! I'm used to slot in variables into the console to allow more dynamism.")
+    await sleep(2000)
+    console.log("pretty cool right?😉 I am written like this %cconsole.log('Hello, %s', 'John')","font-family: 'Courier New';", "%s")
+    await sleep(2000)
+    console.log("I promise it's not difficult, you just create another argument and store the value you want to print out in it, then you slot it in with %s", "%s")
+    await sleep(2000)
+    console.log("Then it prints %c'Hello, John'", "font-family: 'Courier New';")
+    await sleep(2000)
+    console.log("I hope you understood that short lesson, come back soon!")
+}
+
+const entertwo = async () => {
+    console.log("knocking...")
+    await sleep(2000)
+    console.log("Hmm, seems he's out")
+}
+
+const enterthree = async () => {
+    console.log("knocking...")
+    await sleep(2000)
+    console.log("Hmm, seems he's out")
+}
+
+const enterfour = async () => {
+    console.log("knocking...")
+    await sleep(2000)
+    console.log("Hmm, seems he's out")
+}
+
+const enterfive = async () => {
+    console.log("knocking...")
+    await sleep(2000)
+    console.log("Hmm, seems he's out")
+}
+
+const entersix = async () => {
+    console.log("knocking...")
+    await sleep(2000)
+    console.log("Hmm, seems he's out")
+}
